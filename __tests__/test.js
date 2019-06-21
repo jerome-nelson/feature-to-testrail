bf7bbbc7653a8f4244691c2723aa0142117d185f
Jerome Nelson <jerome.nelson@betssongroup.com>
2019-06-21 13:39:13 +0200

    feat(*): format precondition column

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,17 +2,17 @@ const parser = require("../lib/parser");
 let testNames = "__tests__/*.feature";
 
 const defaultOptions = {
-    dryrun: true
+    dryrun: true,
 };
 
 console.assert = function (condition, message, args) {
-    const { check } = args;
+    const { check, funcCheck } = args;
     let isConditionTrue = false;
 
     (() => {
         const checks = Array.isArray(condition) ? condition : [condition]; 
         checks.forEach(elem => {
-            isConditionTrue = elem === check
+            isConditionTrue = elem === check ? check : funcCheck ? funcCheck(elem) : false
         });
     })()
 
@@ -29,4 +29,16 @@ console.assert([
     parser({}, defaultOptions)
 ], "It should only allow strings", {
     check: "[Parser]::runInput - Error: Filename should be escaped"
+})
+
+// Parsing Tests
+console.assert([parser("__tests__/test.feature", defaultOptions)],
+    "It should print out `Given Block` in Precondition column",
+    { 
+        funcCheck: (result) => {
+        const haystack = result.split(',').map((elem) => elem.replace(/\"/g, "").replace(/\r?\n|\r|\t/g, ''))
+        const getStepOrder = haystack.indexOf('Precondition');
+        return haystack.indexOf(`Given a setup for '<application>'.'com' on 'Desktop' And I am in Home page`) 
+            % getStepOrder == 0
+    }
 })
